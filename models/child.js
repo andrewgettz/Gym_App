e6a13f5d491a15209e1763d5739083112eb49f2a
Andrew Gettz <drews_computer@Andrews-MacBook-Pro.local>
2022-08-31 12:34:54 -0400

    updated child.js in model to define data table

---
+++ models/child.js
@@ -0,0 +1,21 @@
+module.exports = function(sequelize, DataTypes) {
+    var Child = sequelize.define('Child', {
+        name_first: DataTypes.STRING,
+        name_last: DataTypes.STRING,
+        age: DataTypes.INTEGER,
+        gender: DataTypes.STRING,
+        allergies: DataTypes.TEXT,
+        dietRestrictions: DataTypes.TEXT,
+        notes: DataTypes.TEXT,
+		photoLink:DataTypes.STRING
+    }); 
+    Child.associate = function(models) {
+        Child.belongsTo(models.Parent, {
+            foreignKey: {allowNull: true,defaultValue:0}
+        }); 
+        Child.hasOne(models.Schedule, {
+            onDelete: 'cascade'
+        });
+    };
+    return Child;
+}; 
