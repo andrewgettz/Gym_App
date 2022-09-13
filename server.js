15f426bba949a584daf6eedf0dabe0a218c96761
mdemougin <mattdemougin@sbcglobal.net>
2022-09-13 08:58:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const expressSession = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require('passport-local');
 
+// Loads environment variables
 require("dotenv").config();
 
 const db = require("./models");
@@ -13,7 +14,17 @@ const { env } = require("process");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//Middleware
+// Middleware
+const apiCheck = (req, res, next) => {
+  const { apiKey } = req.body;
+
+  if (apiKey === env.process.API_KEY) {
+    next();
+  } else {
+    res.status(400);
+  }
+}
+
 const session = {
   secret: process.env.SESSION_SECRET,
   cookie: {},
@@ -30,9 +41,6 @@ app.engine(
   );
   app.set("view engine", "handlebars");
 
-  //Routes 
-
-
 db.sequelize.sync(syncOptions).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
@@ -41,12 +49,8 @@ db.sequelize.sync(syncOptions).then(function() {
   
   module.exports = app;
 
-const strategy = new LocalStrategy(function verify(username, password, apiKey, cb) {
+const strategy = new LocalStrategy(function verify(username, password, cb) {
   db.get('SELECT * FROM users WHERE username = ?', [ username ], (err, user) => {
-    if (apiKey !== env.process.API_KEY){
-      return cb(null, false, { message: 'Incorrect username or password.' });
-    }
-
     if (err) {
       return cb(err); 
     }
@@ -69,7 +73,26 @@ const strategy = new LocalStrategy(function verify(username, password, apiKey, c
   });
 });
 
+const createSaltAndHash = password => new Promise((resolve, reject) => {
+  const salt = crypto.randomBytes(128).toString('base64');
+
+  crypto.pbkdf2(password, salt, ITERATIONS, 256, 'sha256', (err, key) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve({
+        salt,
+        hash: key.toString('hex'),
+      });
+    }
+  });
+});
+
+
 passport.use(strategy);
+app.use(apiCheck);
+
+//Routes 
 
 app.post('/login/password',
   passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
@@ -77,3 +100,39 @@ app.post('/login/password',
     res.redirect('/~' + req.user.username);
   }
 );
+
+app.post('/login/createUser', (req, res) => {
+  const { username, password, name } = req.body;
+
+  let saltAndHash;
+
+  createSaltAndHash(password).then((res) => {
+    saltAndHash = res;
+
+    // Change this for db
+    db.insert('INSERT INTO user (name, username, hashed_password, salt VALUES ?, ?, ?, ?', [name, username, saltAndHash.hash, saltAndHash.salt]);
+
+    res.redirect('/~' + req.user.username);
+  });
+});
+
+app.post('/login/updatePassword', (req, res) => {
+  const { username, password } = req.body;
+
+  db.get('SELECT * FROM users WHERE username = ?', [ username ], (err, user) => {
+    if (err) {
+      res.status(400).json({err});
+    }
+
+    let saltAndHash;
+
+    createSaltAndHash(password).then((res) => {
+      saltAndHash = res;
+
+      // Change this for db
+      db.insert('UPDATE user SET hashed_password = ?, salt = ?', [saltAndHash.hash, saltAndHash.salt]);
+
+      res.redirect('/~' + req.user.username);
+    });
+  });
+});
