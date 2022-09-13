36a2b9b663383497d5da17f2deb41e57b6da6c9e
mdemougin <mattdemougin@sbcglobal.net>
2022-09-12 22:18:51 -0400

    Add passport

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const expressSession = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local');
 
-var db = require("./models");
+require("dotenv").config();
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const db = require("./models");
+const { env } = require("process");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 //Middleware
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
 
 // Handlebars
 app.engine(
@@ -27,3 +40,40 @@ db.sequelize.sync(syncOptions).then(function() {
   });
   
   module.exports = app;
+
+const strategy = new LocalStrategy(function verify(username, password, apiKey, cb) {
+  db.get('SELECT * FROM users WHERE username = ?', [ username ], (err, user) => {
+    if (apiKey !== env.process.API_KEY){
+      return cb(null, false, { message: 'Incorrect username or password.' });
+    }
+
+    if (err) {
+      return cb(err); 
+    }
+
+    if (!user) {
+      return cb(null, false, { message: 'Incorrect username or password.' }); 
+    }
+
+    crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', (err, hashedPassword) => {
+      if (err) { 
+        return cb(err); 
+      }
+      
+      if (!crypto.timingSafeEqual(user.hashed_password, hashedPassword)) {
+        return cb(null, false, { message: 'Incorrect username or password.' });
+      }
+
+      return cb(null, user);
+    });
+  });
+});
+
+passport.use(strategy);
+
+app.post('/login/password',
+  passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+  (req, res) => {
+    res.redirect('/~' + req.user.username);
+  }
+);
